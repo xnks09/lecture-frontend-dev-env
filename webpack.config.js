d0a21544b9ade3c7e8229a6ba11d24a1453fee96
nks <xnks@gsitm.com>
2022-10-04 02:30:30 +0900

    과제

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const webpack = require("webpack")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   mode: "development",
@@ -13,7 +17,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          process.env.NODE_ENV === 'production'
+          ? MiniCssExtractPlugin.loader
+          : "style-loader", "css-loader"
+        ]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
@@ -24,7 +32,7 @@ module.exports = {
         }
       }
     ]
-  }
+  },
   /**
    * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
    * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
@@ -32,4 +40,21 @@ module.exports = {
    * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
    * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
    */
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `Build Time: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+      }
+    }),
+    new CleanWebpackPlugin(),
+    ...(
+      process.env.NODE_ENV === 'production' ? 
+      [new MiniCssExtractPlugin({filename: '[name].css'}) ]
+      : []
+    )
+  ]
 };
