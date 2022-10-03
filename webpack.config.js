6d3c4ece71dba02bcb5cdf0b8c647b89a650f54a
nks <xnks@gsitm.com>
2022-10-04 00:31:21 +0900

    과제

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,22 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/, // .css 확장자로 끝나는 모든 파일
+        use: ["style-loader", "css-loader"], // css-loader를 적용한다
+      },
+      {
+        test: /\.(jpg|png)$/,
+        loader: 'url-loader',
+        options: {
+          publicPath: './dist/', // file-loader와 동일
+          name: '[name].[ext]?[hash]', // file-loader와 동일
+          limit: 10000 //10kb
+        }
+      }
+    ],
+  },
 }
